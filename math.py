95b4373bda78e90aa27a9380285ea3124cd1614d
Tilley <tilleyy8@gmail.com>
2024-10-25 10:10:24 -0600

    Update math.py

--- math.py
+++ math.py
@@ -1,40 +1,45 @@
 import os
 import random
-
-max_value = 12 
-
+max_factor = 12
 os.system("clear")
 
-def generate_problem():
-    num1 = random.randint(1, max_value)
-    num2 = random.randint(1, max_value)
-    if random.choice([True, False]):
-        return (num1, num2, num1 * num2, '*')
-    else:
-        num1 = num1 * num2
-        return (num1, num2, num1 // num2, '/')
+def ask_multiplication():
+    a = random.randint(1, max_factor)
+    b = random.randint(1, max_factor)
+    correct_answer = a * b
+    while True:
+        user_input = input(f"What is {a} * {b}? ")
+        if check_answer(user_input, correct_answer):
+            break
 
-def repeat(problem):
-    uinput = input("\033[94m[ok]\033[0m ")
-    os.system("clear")
-    while (uinput != str(problem[2])):
-        uinput = input(f"What is {problem[0]} {problem[3]} {problem[1]}? ")
-        check(problem, uinput)
-        os.system("clear")
-        problem = generate_problem()
+def ask_division():
+    b = random.randint(1, max_factor)
+    correct_answer = random.randint(1, max_factor) * b
+    a = correct_answer // b
+    while True:
+        user_input = input(f"What is {correct_answer} / {b}? ")
+        if check_answer(user_input, a):
+            break
 
-def check(problem, answer):
-    if answer == str(problem[2]):
-        print("\033[92mCorrect!\033[0m")
-        os.system("sleep 0.5&&clear")
-    elif answer == "":
-        print(f"The answer is {problem[2]}.")
-        repeat(problem)
-    else:
-        print("\033[31mIncorrect.\033[0m Answer is:", problem[2])
-        repeat(problem)
+def check_answer(user_input, correct_answer):
+    if user_input.strip() == "":
+        print(f"The answer is: {correct_answer}")
+        return False
+    try:
+        if int(user_input) == correct_answer:
+            print("\033[92mCorrect!\033[0m")
+            os.system("sleep 0.5 && clear")
+            return True
+        else:
+            print(f"\033[31mIncorrect.\033[0m The answer is: {correct_answer}")
+            return False
+    except ValueError:
+        print("\033[31mInvalid input\033[0m")
+        return False
 
 while True:
-    problem = generate_problem()
-    uinput = input(f"What is {problem[0]} {problem[3]} {problem[1]}? ")
-    check(problem, uinput)
+    operation = random.choice(['multiplication', 'division'])
+    if operation == 'multiplication':
+        ask_multiplication()
+    else:
+        ask_division()
